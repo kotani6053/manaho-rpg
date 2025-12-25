2ad29ad65e621981d82d134349d936b9af3e838d
kotani6053 <yoshioka@e-kotani.co.jp>
2025-12-25 10:48:01 +0900

    Update tailwind.config.js

--- my-study-rpg/tailwind.config.js
+++ my-study-rpg/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // appフォルダの中を見る
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", // componentsフォルダの中を見る
+  ],
   theme: {
     extend: {},
   },
