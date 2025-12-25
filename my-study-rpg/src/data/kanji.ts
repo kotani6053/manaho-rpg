6d16f65c6054e397ada7951989ae32086869c99c
kotani6053 <yoshioka@e-kotani.co.jp>
2025-12-25 11:01:06 +0900

    Delete my-study-rpg/src/data/kanji.ts

--- my-study-rpg/src/data/kanji.ts
+++
@@ -1,6 +0,0 @@
-export const kanjiData = [
-  { question: "「山」のよみかたは？", answer: "やま" },
-  { question: "「川」のよみかたは？", answer: "かわ" },
-  { question: "「空」のよみかたは？", answer: "そら" },
-  { question: "「海」のよみかたは？", answer: "うみ" },
-];
